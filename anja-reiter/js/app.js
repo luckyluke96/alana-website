095d1d4ae3e4558a330a98251e66ca9156a91041
luckyluke96 <lukas.oberfrank6@gmail.com>
2025-06-26 17:28:36 +0200

    initial commit

---
+++ anja-reiter/js/app.js
@@ -0,0 +1,13 @@
+function navToggle() {
+    var mobilemenu = document.getElementById('mobilemenu');
+    mobilemenu.classList.toggle('block');
+    mobilemenu.classList.toggle('hidden');
+
+    var mobilehamburger = document.getElementById('mobilehamburger');
+    mobilehamburger.classList.toggle('block');
+    mobilehamburger.classList.toggle('hidden');
+
+    var mobileclose = document.getElementById('mobileclose');
+    mobileclose.classList.toggle('block');
+    mobileclose.classList.toggle('hidden');
+}
